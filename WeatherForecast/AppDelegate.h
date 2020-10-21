406a68e7eb766199c3ccf7ea956ab644ccd5a2cb
alpturk.bostan <alpturk.bostan@n11.com>
2020-10-21 10:13:22 +0300

    Initial Commit

---
+++ WeatherForecast/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  WeatherForecast
+//
+//  Created by Seda Deliorman on 30/06/16.
+//  Copyright © 2016 N11.com. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ WeatherForecast/MainVC.h
@@ -0,0 +1,15 @@
+//
+//  MainVC.h
+//  WeatherForecast
+//
+//  Created by Seda Deliorman on 30/06/16.
+//  Copyright © 2016 N11.com. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MainVC : UIViewController
+
+
+@end
+

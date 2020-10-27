0fac232c2f0dda0c44a2b46c82cc2982976b63bc
erkutbas <ebas1@binghamton.edu>
2020-10-28 01:27:18 +0300

    Pod Files removed

--- Pods/Target Support Files/Pods-WeatherForeCast-WeatherForeCastUITests/Pods-WeatherForeCast-WeatherForeCastUITests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_WeatherForeCast_WeatherForeCastUITests : NSObject
-@end
-@implementation PodsDummy_Pods_WeatherForeCast_WeatherForeCastUITests
-@end
